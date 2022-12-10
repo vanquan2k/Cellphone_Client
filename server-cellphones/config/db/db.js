f834b9aa771b51d19ca1638a4700f28e20672f05
QuanDang <anhlapro1243@gmail.com>
2022-12-10 13:22:32 +0700

    first commit

---
+++ server-cellphones/config/db/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function connectDB(){
+    // const url = 'mongodb://localhost:27017/shop'
+    // const url = 'mongodb+srv://CaoKhaHieu:<CaoKhaHieu>@cluster0.r9hva.mongodb.net/shop?retryWrites=true&w=majority'
+    const url = 'mongodb+srv://quanprox3:quanprox3@cluster0.whxpsrl.mongodb.net/shop?retryWrites=true&w=majority'
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        console.log("connected to db")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDB;
